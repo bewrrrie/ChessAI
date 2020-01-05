c1a80fa0a59e1a85250637de7a55f9b8ccb5f287
Vadim Leshkov <silverr41@gmail.com>
2020-01-06 02:44:24 +0600

    add current move color tracking

--- src/GameIO.hs
+++ src/GameIO.hs
@@ -5,19 +5,23 @@ import ChessLib
 import Parsing
 import System.IO
 import Control.Monad
+import Data.Maybe (fromMaybe)
 
-gameLoop :: Game -> IO ()
-gameLoop game = do { print game
-                   ; cmd <- getLine
-                   ; if parseQuit cmd
-                     then putStrLn "Finishing game process..."
-                     else do { let move = parseMove cmd
-                             ; let newGame = makeMove game move
-                             ; if isGameFinished newGame
-                               then putStrLn "Finishing game process..."
-                               else gameLoop newGame } }
+-- Main game loop.
+gameLoop :: Color -> Color -> Game -> IO ()
+gameLoop plColor curColor game = do { print game
+                                    ; cmd <- getLine
+                                    ; if parseQuit cmd
+                                      then putStrLn "Finishing game process..."
+                                      else do { let move = parseMove cmd
+                                              ; let newGame = makeMove game move
+                                              ; let newColor = switchColor curColor
+                                              ; if isGameFinished newGame
+                                                then putStrLn "Finishing game process..."
+                                                else gameLoop plColor curColor newGame } }
 
--- IO function for reading input and getting output
+-- IO function for reading input and getting output.
+-- Main entry point to start the game.
 playChess :: IO ()
 playChess = do { hSetBuffering stdout NoBuffering
                ; putStr "Select color ('B'/'W' or 'q' to quit): "
@@ -25,5 +29,5 @@ playChess = do { hSetBuffering stdout NoBuffering
                ; if parseQuit cmd
                  then putStrLn "Finishing game process..."
                  else do {
-                         ; let playerColor = parseColor cmd
-                         ; gameLoop initialGame } }
+                         ; let playerColor = fromMaybe White (parseColor cmd)
+                         ; gameLoop playerColor White initialGame } }
