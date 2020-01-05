1785fddd9ddd5acd4f141d86a95c229fcaf36798
Vadim Leshkov <silverr41@gmail.com>
2020-01-06 04:44:24 +0600

    add ChessAI module

---
+++ src/ChessAI.hs
@@ -0,0 +1,6 @@
+module ChessAI where
+
+import ChessLib
+
+aiDecide :: Game -> Color -> (Int, Int, Int, Int)
+aiDecide _ _ = undefined
--- src/GameIO.hs
+++ src/GameIO.hs
@@ -2,6 +2,7 @@
 module GameIO where
 
 import ChessLib
+import ChessAI
 import Parsing
 import System.IO
 import Control.Monad
@@ -10,19 +11,23 @@ import Data.Maybe (fromMaybe, isNothing)
 -- Piece move inference function.
 -- If it is our turn we parse the move from cmd
 -- otherwise if it is AI's turn move must be inferred by minimax algorithm.
-inferMove :: Color -> Maybe Color -> String -> (Int, Int, Int, Int)
-inferMove playerColor maybeMoveColor cmd = if maybeMoveColor == Just playerColor
-                                           then parseMove cmd
-                                           else parseMove cmd--TODO AI decision
+inferMove :: Game -> Color -> Maybe Color -> String -> (Int, Int, Int, Int)
+inferMove game playerColor (Just color) cmd = if color == playerColor
+                                              then parseMove cmd
+                                              else aiDecide game color
+inferMove _    _            Nothing     _   = error "Move color is Nothing!"
 
 -- Main game loop.
 gameLoop :: Color -> Game -> IO ()
 gameLoop playerColor game = do { print game
-                               ; cmd <- getLine
                                ; let maybeMoveColor = getMoveColor (getGameState game)
+                               ; if maybeMoveColor == Just playerColor
+                                 then putStr "Make your move: "
+                                 else putStr "AI is going to make its decision."
+                               ; cmd <- getLine
                                ; if isNothing maybeMoveColor
                                  then putStrLn "Game is finished.\nFinishing game process..."
-                                 else do { let move = inferMove playerColor maybeMoveColor cmd
+                                 else do { let move = inferMove game playerColor maybeMoveColor cmd
                                          ; let newGame = makeMove game move
                                          ; if isGameFinished newGame
                                            then putStrLn "Game is finished.\nFinishing game process..."
