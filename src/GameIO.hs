f3983b8a96c57fb01adc331112e300f779100a08
Vadim Leshkov <silverr41@gmail.com>
2020-01-05 13:14:51 +0600

    add basic game loop

--- src/GameIO.hs
+++ src/GameIO.hs
@@ -1,7 +1,24 @@
+{-# LANGUAGE BlockArguments #-}
 module GameIO where
 
-import ChessLib (makeMove)
+import ChessLib
+import Parsing
+import System.IO
+import Control.Monad
 
--- IO interface function for reading input and getting output
+gameLoop :: Game -> IO ()
+gameLoop game = do { print game
+                   ; moveStr <- getLine
+                   ; let move = parseMove moveStr
+                   ; let newGame = makeMove game move
+                   ; if isGameFinished newGame
+                     then putStrLn "Finishing game process..." 
+                     else gameLoop newGame }
+
+-- IO function for reading input and getting output
 playChess :: IO ()
-playChess = putStrLn "LOL"
+playChess = do { hSetBuffering stdout NoBuffering
+               ; putStr "Select color (B/W): "
+               ; colorStr <- getLine
+               ; let playerColor = parseColor colorStr
+               ; gameLoop initialGame }
