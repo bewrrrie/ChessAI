82096e2c9ebefa06035c8dad0b7de52d1b452db7
Vadim Leshkov <silverr41@gmail.com>
2020-01-07 19:56:50 +0600

    add random AI

--- src/ChessAI.hs
+++ src/ChessAI.hs
@@ -1,6 +1,29 @@
 module ChessAI where
 
-import ChessLib
+import ChessLib ( Game
+                , Board (..)
+                , Color
+                , Move
+                , CellCoords
+                , getCellCoordinates
+                , getCellPiece
+                , getMaybePieceColor
+                , getGameBoard
+                , filterBoard
+                , getMove
+                , boardSize
+                , isMoveAllowed )
 
 aiDecide :: Game -> Color -> Move
-aiDecide _ _ = undefined
+aiDecide game moveColor = getMove srcCell destCell
+  where pick seed (Board cells) = cells !! (seed `mod` length cells)
+        board     = getGameBoard game
+        srcCell   = pick rndIdx ourPiecesSubBoard
+        destCell  = pick rndIdx $ filterBoard ( isMoveAllowed moveColor board .
+                                                getMove srcCell ) board
+        rndIdx    = 0
+        destCoord = getCellCoordinates destCell
+        ourPiecesSubBoard = filterBoard ( \cell -> Just moveColor == (getMaybePieceColor . getCellPiece) cell
+                                                && 0 < (boardSize . canMoveToSubBoard) cell ) board
+        canMoveToSubBoard cell = filterBoard ( isMoveAllowed moveColor board .
+                                               getMove cell ) board
